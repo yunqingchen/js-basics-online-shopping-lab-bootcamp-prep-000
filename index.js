bd26aeba83e1435f045a6a8fc5be6cec21606561
Clark Chen <yyunqingchenn@gmail.com>
2019-07-10 23:42:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function viewCart() {
 
 function total(){
   total = 0;
-  for (var i = 0; i<cart.length; i++);
+  for (var i = 0; i<cart.length; i++){
   total += cart[i].itemPrice
-  
+  }
+return total;
 }

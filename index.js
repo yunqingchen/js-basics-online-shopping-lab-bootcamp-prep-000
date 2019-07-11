d79e5077fc28ae1f5a500ce0840525cb73c2d040
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 00:12:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function placeOrder(cardNumber){
   if (typeof(cardNumber) === "undefined") {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    return `Your total cost is $` + total() + `, which will be charged to the card` + cardNumber + `.`;
+    return `Your total cost is $` + total() + `, which will be charged to the card ` + cardNumber + `.`;
   }
   cart = [];
 }

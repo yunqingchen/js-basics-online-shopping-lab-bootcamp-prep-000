d727e3e722a69cbe42ed34492f152a773bcd7e8a
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 00:18:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ function removeFromCart(item){
 function placeOrder(cardNumber){
   if (cardNumber) {
     return `Your total cost is $` + total() + `, which will be charged to the card ` + cardNumber + `.`
-    }
     for (var i = cart.length; i>=0; i--){
       cart.pop();
     }

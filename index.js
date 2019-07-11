4f9b32dda42c243eb0aa940524fa39395e4775c7
Clark Chen <yyunqingchenn@gmail.com>
2019-07-11 00:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function removeFromCart(item){
 }
 
 function placeOrder(cardNumber){
-  if (cardNumber) {
+  if (typeof(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     return `Your total cost is $` + total() + `, which will be charged to the card ` + cardNumber + `.`;
